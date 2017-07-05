88138cfb353e6daea8c7f556d3176aa1c1d48603
Federico Ortiz <fede@stuffsandstuffs.coldspring.marian.edu>
2017-07-05 16:16:57 -0400

    Log to the console when a note is clicked

--- src/Note.js
+++ src/Note.js
@@ -1,21 +1,25 @@
 import React from 'react'
 
 const Note = (props) => {
-  return (
-    <a>
-      <li>
-        <div className="note">
-          <div className="note-title">
-            {props.note.title}
-          </div>
-          <div className="note-body">
-            <p>
-              {props.note.body}
-            </p>
-          </div>
-        </div>
-      </li>
-    </a>
+    const handleClick = () => {
+        console.log('clicky clicky')
+    }
+
+    return (
+        <a onClick={handleClick}>
+        <li>
+            <div className="note">
+            <div className="note-title">
+                {props.note.title}
+            </div>
+            <div className="note-body">
+                <p>
+                {props.note.body}
+                </p>
+            </div>
+            </div>
+        </li>
+        </a>
   )
 }
 
